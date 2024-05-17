b4753db6adea1418ffdfaad0e816c63173f36f49
zacharybaca <zachary.baca@msn.com>
2024-05-17 17:11:53 -0500

    made some changes

--- the-calculator-app.py
+++ the-calculator-app.py
@@ -4,17 +4,17 @@ Task 3: Ensure your program can handle division by zero and other potential erro
 
 # Task 1
 def add(num1, num2):
-    print(num1 + num2)
+    print('The Sum of Your Two Values is: ', num1 + num2)
 
 def subtract(num1, num2):
-    print(num1 - num2)
+    print('The Difference Between Your Two Values is: ', num1 - num2)
 
 def multiply(num1, num2):
-    print(num1 * num2)
+    print('The Product of Your Two Values is: ', num1 * num2)
 
 def divide(num1, num2):
     if num2 != 0:
-        print(num1 / num2)
+        print(f'{num2} is Divisible by {num1} This Many Times: ', num1 / num2)
     else:
         print('Cannot Divide By Zero')
         number_one = int(input("Please Input a Value for Your First Number: "))
@@ -24,7 +24,7 @@ def divide(num1, num2):
 # Task 2
 number_one = int(input("Please Input a Value for Your First Number: "))
 number_two = int(input("Please Input a Value for Your Second Number: "))
-operation_choice = input("Would You Like to Add, Subtract, Multiply, or Divide Your Two Values?").lower()
+operation_choice = input("Would You Like to Add, Subtract, Multiply, or Divide Your Two Values? ").lower()
 
 # Task 3
 def calculate(operation, num1, num2):
@@ -43,7 +43,7 @@ def calculate(operation, num1, num2):
         calculate(operation, number_one, number_two)
     elif operation != "add" or operation != "subtract" or operation != "multiply" or operation != "divide":
         print('Please Enter a Valid Operation')
-        operation_choice = input("Would You Like to Add, Subtract, Multiply, or Divide Your Two Values?")
+        operation_choice = input("Would You Like to Add, Subtract, Multiply, or Divide Your Two Values? ")
         calculate(operation_choice, num1, num2)
     else:
         raise Exception("Unknown Error")
