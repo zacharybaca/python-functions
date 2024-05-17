9694a720198b37dfa1969aaa6e0d10d430563be1
Zachary Baca <zachary.baca@msn.com>
2024-05-17 12:43:16 -0500

    Update the-calculator-app.py

--- the-calculator-app.py
+++ the-calculator-app.py
@@ -17,13 +17,13 @@ def divide(num1, num2):
         print(num1 / num2)
     else:
         print('Cannot Divide By Zero')
-        number_one = input("Please Input a Value for Your First Number: ")
-        number_two = input("Please Input a Value for Your Second Number: ")
+        number_one = int(input("Please Input a Value for Your First Number: "))
+        number_two = int(input("Please Input a Value for Your Second Number: "))
         divide(number_one, number_two)
 
 # Task 2
-number_one = input("Please Input a Value for Your First Number: ")
-number_two = input("Please Input a Value for Your Second Number: ")
+number_one = int(input("Please Input a Value for Your First Number: "))
+number_two = int(input("Please Input a Value for Your Second Number: "))
 operation_choice = input("Would You Like to Add, Subtract, Multiply, or Divide Your Two Values?")
 
 # Task 3
@@ -48,4 +48,5 @@ def calculate(operation, num1, num2):
     else:
         raise Exception("Unknown Error")
         
-    calculate(operation_choice, number_one, number_two)
+calculate(operation_choice, number_one, number_two)
+
